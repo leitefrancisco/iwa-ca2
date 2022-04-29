d2919a2b4f3cf05037431a46fb8c76053e494e0e
leitefrancisco <franciscolmf@outlook.com>
2022-04-29 21:03:01 +0000

    deleted image model and controller

--- models/recipe.js
+++ models/recipe.js
@@ -3,10 +3,15 @@ const mongoose = require("mongoose");
 const recipeSchema = new mongoose.Schema
     ({
         name: { type: String, unique: true, dropDups: true,lowercase: true},
-        tags: [{
+        ingredients: [{
             type: String
         }],
-        instructions: String  
+        instructions: String,
+        img:
+            {
+                data: Buffer,
+                contentType: String
+            } 
     })
 
 module.exports = mongoose.model('Recipe', recipeSchema);
